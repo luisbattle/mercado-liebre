7aea016387adbd5f45901d1d95c633127c175d4b
Luis <luisbattle@hotmail.com.ar>
2020-10-02 13:03:09 -0300

    se agrega pagina estilos css en home y se comienza con register

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ app.get("/login", function (req, res) {
   let file = path.resolve("vistas/login.html");
   res.sendFile(file);
 });
+app.get("/register", function (req, res) {
+  let file = path.resolve("vistas/register.html");
+  res.sendFile(file);
+});
 
 app.get("*", function (req, res) {
   if (req.url.endsWith(".css")) {
